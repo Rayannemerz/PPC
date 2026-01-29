f767cab07148f6501e673ffd0f3543fe51bf3f51
Rayannemerz <rayanne.merzouki@insa-lyon.fr>
2026-01-29 21:28:24 +0100

    Update

--- prey.py
+++ prey.py
@@ -8,9 +8,9 @@ def prey_process(nb_herbe, nb_prey, secheresse_status, grid_status, prey_pos):
     
     # Position initiale aléatoire
     position = random.randint(0, 399)
-    prey_pos[pid] = position
+    prey_pos[pid] = position #enregistre la position dans le dict partagé
     
-    last_repro = time.time() - 10
+    last_repro = time.time() - 10 # pour permettre une reproduction tout les 10sec
     try:
         while energie > 0:
             # Déplacement aléatoire vers une case adjacente
@@ -37,7 +37,7 @@ def prey_process(nb_herbe, nb_prey, secheresse_status, grid_status, prey_pos):
                     # La case perd l'herbe seulement si la proie a mangé
                     grid_status[position] = 1
             
-            # --- REPRODUCTION ---
+            #reproduction si assez d'énergie et de temps écoulé
             if energie > 90 and (time.time() - last_repro) > 10 and nb_herbe.value > 10:
                 energie -= 40
                 last_repro = time.time()
@@ -45,13 +45,13 @@ def prey_process(nb_herbe, nb_prey, secheresse_status, grid_status, prey_pos):
                     # Si l'espèce est éteinte (sécurité), ne pas reproduire
                     if nb_prey.value == 0:
                         raise RuntimeError("espèce PREY éteinte")
-                    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    soc.settimeout(1.0)
-                    soc.connect(('localhost', 8080))
-                    soc.sendall(b"PREY")
-                    soc.close()
+                    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # crée une socket TCP et ipv4
+                    soc.settimeout(1.0)  # fixe un délai max pour la connexion
+                    soc.connect(('localhost', 8080))  # se connecte au serveur local (port 8080)
+                    soc.sendall(b"PREY")  # envoie le message de reproduction binaire
+                    soc.close()  # ferme la connexion
                     print(f"[PROIE {pid}] Un petit est né !")
-                except Exception as e:
+                except Exception as e: #affichage erreur reproduction
                     print(f"[PROIE {pid}] Échec reproduction : {e}")
             time.sleep(2)
             energie -= 10
@@ -66,9 +66,10 @@ if __name__ == '__main__':
     SyncManager.register("get_herbe", proxytype=ValueProxy)
     SyncManager.register("get_prey", proxytype=ValueProxy)
     SyncManager.register("get_secheresse", proxytype=ValueProxy)
-    SyncManager.register("get_grid")
+    SyncManager.register("get_grid") #enregistre coté client pour pouvoir appeler m.get...
     SyncManager.register("get_prey_pos")
+    #lire et partager depuis un autre process (valueProxy)
     
-    m = SyncManager(address=('127.0.0.1', 50000), authkey=b'abc')
+    m = SyncManager(address=('127.0.0.1', 50000), authkey=b'abc') 
     m.connect()
     prey_process(m.get_herbe(), m.get_prey(), m.get_secheresse(), m.get_grid(), m.get_prey_pos())
